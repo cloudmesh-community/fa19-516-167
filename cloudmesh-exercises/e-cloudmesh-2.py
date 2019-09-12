5126795276dd00e84dcf7679b033270c711e1311
william-screen <26144454+william-screen@users.noreply.github.com>
2019-09-12 18:09:44 -0400

    Initial commit

---
+++ cloudmesh-exercises/e-cloudmesh-2.py
@@ -0,0 +1,46 @@
+# fa19-516-167 E.Cloudmesh.Common.2
+
+# Imports
+from cloudmesh.common.dotdict import dotdict
+from cloudmesh.common.FlatDict import FlatDict #there is a typo in the book 'Flatdict'
+from cloudmesh.common.Printer import Printer
+
+# Sample JSON data
+data = [
+  {
+  "org": "Amazon",
+  "address": {
+    "street1": "123 aws avenue",
+    "city": "Seattle",
+    "zip": "12345",
+    "country": "United States",
+    "countryCode": "US"
+  }},
+  {
+  "org": "Microsoft",
+  "address": {
+    "street1": "456 azure street",
+    "city": "Redmond",
+    "zip": "54321",
+    "country": "United States",
+    "countryCode": "US"
+  }}
+]
+
+# Convert to dotdict and Print JSON data
+dotdict_data = dotdict(data[0])
+print(type(dotdict_data))
+print(dotdict_data)
+
+# Convert to FlatDict and Print JSON data
+flatdict_data = FlatDict(data[0])
+print(type(flatdict_data))
+print(flatdict_data)
+
+# Printing Dicts
+table = Printer.flatwrite(data, sort_keys=["org"]
+  ,order=["org", "address.street1", "address.city"]
+  ,header=["Name", "Street", "City"]
+  ,output='table')
+
+print(table)
