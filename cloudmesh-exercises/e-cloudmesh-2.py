5507f0625b3331bdd3dc7d1957b0250bf1a11508
william-screen <26144454+william-screen@users.noreply.github.com>
2019-09-13 08:15:17 -0400

    code cleanup

--- cloudmesh-exercises/e-cloudmesh-2.py
+++ cloudmesh-exercises/e-cloudmesh-2.py
@@ -3,8 +3,6 @@
 
 # Imports
 from cloudmesh.common.dotdict import dotdict
-from cloudmesh.common.FlatDict import FlatDict #there is a typo in the book 'Flatdict'
-from cloudmesh.common.Printer import Printer
 
 # Sample JSON data
 data = [
@@ -32,16 +30,3 @@ data = [
 dotdict_data = dotdict(data[0])
 print(type(dotdict_data))
 print(dotdict_data)
-
-# Convert to FlatDict and Print JSON data
-flatdict_data = FlatDict(data[0])
-print(type(flatdict_data))
-print(flatdict_data)
-
-# Printing Dicts
-table = Printer.flatwrite(data, sort_keys=["org"]
-  ,order=["org", "address.street1", "address.city"]
-  ,header=["Name", "Street", "City"]
-  ,output='table')
-
-print(table)
